d993e68ca8252f53bb6067a8b510679ca625b184
anskydev <dev@ansky.sh>
2019-10-14 11:39:56 -0600

    create route for email verification

--- lib/disco_web/router.ex
+++ lib/disco_web/router.ex
@@ -1,13 +1,13 @@
 defmodule DiscoWeb.Router do
   use DiscoWeb, :router
 
-  # pipeline :browser do
-  #   plug(:accepts, ["html"])
-  #   plug(:fetch_session)
-  #   plug(:fetch_flash)
-  #   plug(:protect_from_forgery)
-  #   plug(:put_secure_browser_headers)
-  # end
+  pipeline :browser do
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+  end
 
   pipeline :api do
     plug(:accepts, ["json"])
@@ -24,5 +24,11 @@ defmodule DiscoWeb.Router do
       socket: DiscoWeb.UserSocket,
       interface: :playground
     )
+  end
+
+  scope "/verify/:token" do
+    pipe_through(:browser)
+
+    get("/", DiscoWeb.RegisterVerification, :index)
   end
 end

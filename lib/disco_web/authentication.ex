70b0a864000c60938bdee55d6b9f05057e06b4bb
anskydev <dev@ansky.sh>
2019-10-15 19:50:23 -0600

    update Aunthentication functions

--- lib/disco_web/authentication.ex
+++ lib/disco_web/authentication.ex
@@ -1,12 +1,24 @@
 defmodule DiscoWeb.Authentication do
   @user_salt System.get_env("USER_SALT")
 
-  def sign(data) do
-    Phoenix.Token.sign(DiscoWeb.Endpoint, @user_salt, data)
+  def sign(data, session_type \\ :login) do
+    opts = if session_type == :register, do: [key_length: 16], else: []
+    Phoenix.Token.sign(DiscoWeb.Endpoint, @user_salt, data, opts)
   end
 
   def verify(token, session_type \\ :login) do
-    max_age = if session_type == :register, do: 24 * 3600, else: 30 * 24 * 3600
-    Phoenix.Token.verify(DiscoWeb.Endpoint, @user_salt, token, max_age: max_age)
+    opts =
+      if session_type == :register do
+        [
+          key_length: 16,
+          max_age: 24 * 3600
+        ]
+      else
+        [
+          max_age: 30 * 24 * 3600
+        ]
+      end
+
+    Phoenix.Token.verify(DiscoWeb.Endpoint, @user_salt, token, opts)
   end
 end

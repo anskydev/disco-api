24c90409d8f7f6271d3901128cd3713fd6655cd8
anskydev <dev@ansky.sh>
2019-10-17 22:02:44 -0600

    update Disco.Repo config

--- lib/disco/repo.ex
+++ lib/disco/repo.ex
@@ -1,5 +1,7 @@
 defmodule Disco.Repo do
-  use Ecto.Repo, otp_app: :disco
+  use Ecto.Repo,
+    otp_app: :disco,
+    adapter: Ecto.Adapters.Postgres
 
   @doc """
   Dynamically loads the repository url from the

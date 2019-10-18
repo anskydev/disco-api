8819d176576129841b53ec93aee61c57991e01fd
anskydev <dev@ansky.sh>
2019-10-17 22:03:18 -0600

    update User changeset

--- lib/disco/accounts/user.ex
+++ lib/disco/accounts/user.ex
@@ -2,8 +2,6 @@ defmodule Disco.Accounts.User do
   @moduledoc false
   use Ecto.Schema
   import Ecto.Changeset
-  alias Disco.Accounts.User
-  alias Disco.Repo
 
   schema "users" do
     field(:email, :string)
@@ -14,44 +12,7 @@ defmodule Disco.Accounts.User do
     timestamps()
   end
 
-  def update_changeset(id, attrs) do
-    user = Disco.Accounts.lookup(id)
-
-    case attrs do
-      %{name: _} ->
-        user
-        |> cast(attrs, [:name])
-        |> validate_length(:name, min: 3, max: 18)
-        |> change(attrs)
-        |> Repo.update!()
-
-      %{email: _} ->
-        user
-        |> cast(attrs, [:email])
-        |> unique_constraint(:email)
-        |> validate_change(:email, &valid_email?/2)
-        |> change(attrs)
-        |> Repo.update!()
-
-      %{password: _} ->
-        user
-        |> cast(attrs, [:password])
-        |> validate_length(:password, min: 5, max: 300)
-        |> change(attrs)
-        |> Repo.update!()
-
-      %{verified: _} ->
-        user
-        |> cast(attrs, [:verified])
-        |> change(attrs)
-        |> Repo.update()
-
-      _ ->
-        {:error, "incorrect field(s) to update"}
-    end
-  end
-
-  def register_changeset(%User{} = user, attrs) do
+  def changeset(user, attrs) do
     user
     |> cast(attrs, [:name, :email, :password])
     |> validate_required([:name, :email, :password])
@@ -59,7 +20,6 @@ defmodule Disco.Accounts.User do
     |> validate_length(:name, min: 3, max: 18)
     |> validate_length(:password, min: 5, max: 300)
     |> unique_constraint(:email)
-    |> Repo.insert()
   end
 
   defp valid_email?(_, email) do

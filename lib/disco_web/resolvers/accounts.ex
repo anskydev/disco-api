e048a6f038c51a2a2e0870ef10c30d1f9971c8fe
anskydev <dev@ansky.sh>
2019-10-17 22:04:01 -0600

    update accounts GQL resolvers

--- lib/disco_web/resolvers/accounts.ex
+++ lib/disco_web/resolvers/accounts.ex
@@ -8,22 +8,24 @@ defmodule DiscoWeb.Resolvers.Accounts do
         token = Authentication.sign(%{id: user.id})
         {:ok, %{token: token, user: user}}
 
-      _ ->
+      :error ->
         {:error, "incorrect email or password"}
     end
   end
 
   def register(_, args, _) do
-    case Accounts.register(args) do
-      {:ok, %User{name: name, email: email, id: id}} ->
-        token = Authentication.sign(%{id: id}, :register)
-        verification_url = Helpers.register_verification_url(DiscoWeb.Endpoint, :index, token)
+    with {:ok, %User{name: name, email: email, id: id}} <- Accounts.create_user(args) do
+      token = Authentication.sign(%{id: id}, :register)
+      verification_url = Helpers.register_verification_url(DiscoWeb.Endpoint, :index, token)
 
-        Mailer.send_verification_email(email, name, verification_url)
-        {:ok, %{name: name, email: email}}
+      Mailer.send_verification_email(email, name, verification_url)
+      {:ok, %{user: %{name: name, email: email}}}
+    end
+  end
 
-      {:error, message} ->
-        {:error, message}
+  def update(_, args, %{context: %{current_user: current_user}}) do
+    case Accounts.update_user(current_user, args) do
+      {:ok, user} -> {:ok, %{user: %{name: user.name, email: user.email}}}
     end
   end
 
@@ -33,5 +35,12 @@ defmodule DiscoWeb.Resolvers.Accounts do
 
   def me(_, _, _) do
     {:ok, nil}
+  end
+
+  def verify_password(_, %{password: password}, %{context: %{current_user: current_user}}) do
+    case Comeonin.Ecto.Password.valid?(password, current_user.password) do
+      true -> {:ok, true}
+      _ -> {:ok, false}
+    end
   end
 end

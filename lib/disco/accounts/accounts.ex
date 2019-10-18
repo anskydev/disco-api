cd4eb13013badc5d7689c40e8020d733f28f16b4
anskydev <dev@ansky.sh>
2019-10-17 22:03:06 -0600

    update Accounts context functions

--- lib/disco/accounts/accounts.ex
+++ lib/disco/accounts/accounts.ex
@@ -20,16 +20,6 @@ defmodule Disco.Accounts do
     end
   end
 
-  def register(user_data) do
-    case User.register_changeset(%User{}, user_data) do
-      {:ok, user} ->
-        {:ok, user}
-
-      {:error, _} ->
-        {:error, "email is already in use."}
-    end
-  end
-
   def verify_account(user_id) do
     user = Disco.Accounts.lookup(user_id)
 
@@ -40,5 +30,101 @@ defmodule Disco.Accounts do
 
   def lookup(id) do
     Repo.get_by(User, id: id)
+  end
+
+  alias Disco.Accounts.User
+
+  @doc """
+  Returns the list of users.
+
+  ## Examples
+
+      iex> list_users()
+      [%User{}, ...]
+
+  """
+  def list_users do
+    Repo.all(User)
+  end
+
+  @doc """
+  Gets a single user.
+
+  Raises `Ecto.NoResultsError` if the User does not exist.
+
+  ## Examples
+
+      iex> get_user!(123)
+      %User{}
+
+      iex> get_user!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_user!(id), do: Repo.get!(User, id)
+
+  @doc """
+  Creates a user.
+
+  ## Examples
+
+      iex> create_user(%{field: value})
+      {:ok, %User{}}
+
+      iex> create_user(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a user.
+
+  ## Examples
+
+      iex> update_user(user, %{field: new_value})
+      {:ok, %User{}}
+
+      iex> update_user(user, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_user(%User{} = user, attrs) do
+    user
+    |> User.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a User.
+
+  ## Examples
+
+      iex> delete_user(user)
+      {:ok, %User{}}
+
+      iex> delete_user(user)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_user(%User{} = user) do
+    Repo.delete(user)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking user changes.
+
+  ## Examples
+
+      iex> change_user(user)
+      %Ecto.Changeset{source: %User{}}
+
+  """
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
   end
 end
